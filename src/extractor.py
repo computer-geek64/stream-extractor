e9fcadb0e3249bbf2905742f1c4a0842e1b78ee2
Ashish D'Souza <sudouser512@gmail.com>
2020-05-06 17:18:46 -0400

    Update

--- src/extractor.py
+++ src/extractor.py
@@ -16,9 +16,11 @@ if len(stream_sources) > 0:
     stream_source = None
     if not selection.isnumeric() or int(selection) < 0 or len(stream_sources) <= int(selection):
         stream_source = stream_sources[int(selection)]
+else:
+    print('\r[!] No streams found   ')
 
 if len(subtitle_sources) > 0:
-    print('\r[+] Subtitles found:   ')
+    print('[+] Subtitles found:')
     for i in range(len(subtitle_sources)):
         print(str(i) + ': ' + subtitle_sources[i]['url'])
 
@@ -26,3 +28,9 @@ if len(subtitle_sources) > 0:
     subtitle_source = None
     if not selection.isnumeric() or int(selection) < 0 or len(subtitle_sources) <= int(selection):
         subtitle_source = subtitle_sources[int(selection)]
+else:
+    print('[!] No subtitles found')
+
+
+if stream_source:
+    pass
