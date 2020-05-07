1d6a4cf8e6fe3c429ba2209bac4d8caea4674cf3
Ashish D'Souza <sudouser512@gmail.com>
2020-05-06 20:18:02 -0400

    Update

--- src/config.py
+++ src/config.py
@@ -3,3 +3,8 @@
 
 BROWSERMOB_PROXY = ''  # Absolute filepath for browsermob-proxy binary
 FIREFOX_PROFILE = ''  # Absolute filepath for firefox selenium profile
+HEADLESS = True  # Run firefox in headless mode
+SEARCH_PARAMETERS = [  # Search parameters for streams
+    '.m3u8', '.mp4', '.mkv', '.mpg', '.mpeg', '.mp2', '.mpv', '.mov', '.avi', '.m4p', '.m4v', '.webm', '.mp3', '.wav',
+    '.ogg', '.vob'
+]
